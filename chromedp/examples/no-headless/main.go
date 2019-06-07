e33d942cddaa3b3e1510499dffacbea80ebfae67
catalan <catalan@adobe.com>
2019-06-07 15:21:52 +0200

    Added example of chromedp usage *without* headless

---
+++ chromedp/examples/no-headless/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	dir, err := ioutil.TempDir("", "chromedp-example")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	opts := []chromedp.ExecAllocatorOption{
+		chromedp.NoFirstRun,
+		chromedp.NoDefaultBrowserCheck,
+		// chromedp.Headless,
+		chromedp.DisableGPU,
+		chromedp.UserDataDir(dir),
+	}
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// also set up a custom logger
+	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	// run task list
+	var buf []byte
+	err = chromedp.Run(taskCtx, screenshot(`https://www.golang.org/`, `#footer`, &buf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// save the screenshot to disk
+	if err = ioutil.WriteFile("screenshot.png", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+
+	path := filepath.Join(dir, "DevToolsActivePort")
+	bs, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	lines := bytes.Split(bs, []byte("\n"))
+	fmt.Printf("DevToolsActivePort has %d lines\n", len(lines))
+}
+
+func screenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.WaitVisible(sel, chromedp.ByID),
+		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
+	}
+}
